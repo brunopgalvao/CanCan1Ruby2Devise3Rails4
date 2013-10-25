27e64da740fe01d0adfda73c6eb0184ad379561d
brunopgalvao <brunopgalvao@gmail.com>
2013-10-25 19:09:07 -0400

    CanCan 1 :: Ruby 2 :: Devise 3 :: Rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Joiiy::Application.config.secret_key_base = 'f76e8681051c7bee19a331a8b5e86a05efb6cb93a6a7bea5b227332c47e7498490a9f6856af320314fc4bb81978317c5b207ab0f0f1ae0621147508f82488296'

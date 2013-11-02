8277a33d24dc8bb7767eeb523791a7abdfdff30a
brunopgalvao <brunopgalvao@gmail.com>
2013-11-02 11:34:51 -0400

    adding cancan roles

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -28,5 +28,21 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/ryanb/cancan/wiki/Defining-Abilities
+    user ||= User.new # guest user
+ 
+    if user.role? :super_admin
+      can :manage, :all
+    elsif user.role? :product_admin
+      can :manage, [Product, Asset, Issue]
+    elsif user.role? :product_team
+      can :read, [Product, Asset]
+      # manage products, assets he owns
+      can :manage, Product do |product|
+        product.try(:owner) == user
+      end
+      can :manage, Asset do |asset|
+        asset.assetable.try(:owner) == user
+      end
+    end
   end
 end
